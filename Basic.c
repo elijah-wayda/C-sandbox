3b0dfefd435b6a81aab3f6e034da1b3e78b6b4c9
elijah-wayda <ewayda@dcsstudents.net>
2014-11-17 13:32:08 -0500

    Create Basic.c
    Truly, the next great breakthrough in computer science.

---
+++ Basic.c
@@ -0,0 +1,11 @@
+#import<stdio.h>
+
+int main(int argc, char *argv[])
+{
+  char name[] = "Dave";
+  int age = 23;
+  char job[] = "Pilot";
+  printf("Your name is %s, you're %d years old, and you're a %s.", name, age, job);
+  
+  return 0;
+}
